951fe7c03bf58942df65727dbcd4db4b2baa92b4
Алексей Ершов <alexa3mail@yandex.ru>
2023-06-22 18:44:47 +0300

    Add some voice commands for fun

--- commands/fun/ege.js
+++ commands/fun/ege.js
@@ -5,18 +5,6 @@ module.exports = {
         .setName('ege')
         .setDescription('Get ege results.'),
     async execute(interaction) {
-
-        const Http = new XMLHttpRequest();
-        const url = 'https://checkege.rustest.ru';
-        Http.open("GET", url);
-        Http.send();
-
-        Http.onreadystatechange = (e) => {
-            if (this.readyState === 4 && this.status === 200) {
-                console.log(Http.responseText)
-            }
-        }
-
         let egePoints = {
             "profMath": 23,
             "rusLang": 34
