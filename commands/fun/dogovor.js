44a84ae2f262b8e5b2408a472d588378de903d94
Alexey Ershov <alexa3mail@yandex.ru>
2023-07-25 16:58:47 +0300

    Expanded upon /dogovor command

--- commands/fun/dogovor.js
+++ commands/fun/dogovor.js
@@ -1,11 +1,49 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder } from 'discord.js';
 
 const data = new SlashCommandBuilder()
 	.setName('dogovor')
-	.setDescription('Replies with dogovor.');
+	.setDescription('Dogovor?');
 
 const execute = async (interaction) => {
-	await interaction.reply('Dogovor!');
+
+	const yes = new ButtonBuilder()
+		.setCustomId('yes')
+		.setLabel('Yes')
+		.setStyle(ButtonStyle.Success);
+	
+	const no = new ButtonBuilder()
+	.setCustomId('no')
+	.setLabel('No')
+	.setStyle(ButtonStyle.Danger);
+
+	const actionRow = new ActionRowBuilder()
+		.addComponents(yes, no);
+	
+	const response = await interaction.reply({
+		content: 'Dogovor?',
+		components: [actionRow]
+	});
+
+	const collectorFilter = (i) => {i.user.id === interaction.user.id};
+
+	try {
+		const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+		if (confirmation.customId === 'yes') {
+			await confirmation.update({
+				content: 'Dogovor!',
+				components: []
+			});
+		}
+		if (confirmation.customId === 'no') {
+			await confirmation.update({
+				content: 'No dogovor 😢',
+				components: []
+			});
+		}
+	} catch (e) {
+		await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+	}
 }
 
 export { data, execute };
