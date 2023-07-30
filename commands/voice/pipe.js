ac2beed3c7e4e77012361adc4f5f2ed408921e9a
Alexey Ershov <alexa3mail@yandex.ru>
2023-07-30 22:51:00 +0300

    Add /pipe command

---
+++ commands/voice/pipe.js
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from 'discord.js';
+import playAudioFromFile from '../../utils/playAudioFromFile.js';
+
+const data = new SlashCommandBuilder()
+.setName('pipe')
+.setDescription('Plays pipe sound.');
+
+const execute = async (interaction) => {
+    await interaction.deferReply();
+    try {
+		await playAudioFromFile(interaction, 'assets/audio/pipe.mp3');
+		await interaction.deleteReply();
+	} catch (error) {
+		interaction.editReply('Caught error while executing the commnad. Try again.');
+		console.log(error);
+	}
+}
+
+export { data, execute };
